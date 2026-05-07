7de3816e683bd411b1aee9aba4607104b1438afa
SomarExpress <admin@somar-express.com>
2026-05-06 18:16:17 -0600

    Update delivery-sw.js

--- delivery-sw.js
+++ delivery-sw.js
@@ -1,5 +1,5 @@
 // SERVICE WORKER — Somar Rider PWA
-const CACHE = 'somar-rider-v3.0.5';
+const CACHE = 'somar-rider-v3.0.6';
 const STATIC = ['./index.html', './manifest-delivery.json'];
 
 self.addEventListener('install', e => {
